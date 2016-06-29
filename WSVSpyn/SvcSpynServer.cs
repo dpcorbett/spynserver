a63ad27f47c7348e662c2e94b3dff205ff6c2aa9
dpcorbett <dpcomputing@gmail.com>
2016-06-29 23:48:55 +0930

    Update sync pair view.

--- WSVSpyn/SvcSpynServer.cs
+++ WSVSpyn/SvcSpynServer.cs
@@ -1,19 +1,7 @@
 ﻿using SharpTools.TCPSocketServer;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
-using System.Net.Sockets;
-using System.Net;
-using System.Security.AccessControl;
-using System.Threading;
 
 
 namespace WSVSpyn
