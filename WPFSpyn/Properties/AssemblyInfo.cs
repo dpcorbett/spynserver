63851cc069f0f3da5cf5608c67aec57e6fc56306
dpcorbett <dpccomputing@gmail.com>
2016-09-18 17:29:09 +0930

    Update version.

--- WPFSpyn/Properties/AssemblyInfo.cs
+++ WPFSpyn/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.0.5")]
+[assembly: AssemblyFileVersion("1.3.1.5")]
